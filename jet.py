097d5d12c699f369dc87a4d58ae8331361f962c9
Alex Y. Wagner <alexander.y.wagner@gmail.com>
2023-07-28 11:17:39 +0800

    Parameters of Rei's relativistic vs non-relativistic jet FB project

--- jet.py
+++ jet.py
@@ -3,18 +3,20 @@ import physconst as pc
 import norm
 
 p = jp.JetParameters(
-    power=1.0e42,
-    chi=1.6,
-    pratio=1.0,
-    lorentz=9.0,
-    r_jet=0.3,
-    alpha=30,
-    gamma_rhd=1.33333333,
-    dens_ambient=0.1,
-    temp_ambient=1.e7,
-    gamma_hd=1.6666666666,
-    input_param='chi',
-    norm_code=norm.PhysNorm(x=pc.pc, v=pc.c, dens=0.6034 * pc.amu,
+    power=1.0e45,             # cgs
+    chi=0.69444444,           # dimensionless
+    pratio=1.0,               # dimensionless
+    lorentz=5.0,              # dimensionless
+    r_jet=40.,               # code units
+    alpha=0,                  # degrees
+    gamma_rhd=1.33333333,     # dimensionless
+    dens_ambient=0.3,         # code units
+    temp_ambient=1.e7,        # Kelvin
+    gamma_hd=1.6666666666,    # dimensionless
+    input_param='chi',        # char
+
+    # Define code units
+    norm_code=norm.PhysNorm(x=pc.kpc, v=pc.c, dens=0.6034 * pc.amu,
                             temp=pc.c ** 2 * pc.amu / pc.kboltz, curr=1)
 )
 
